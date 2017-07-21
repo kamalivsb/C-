d07577f04c38372386b9352102b75117a8d4bcc5
kamalivsb <30316996+kamalivsb@users.noreply.github.com>
2017-07-21 11:55:54 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,17 @@
+# C-
+#include<stdio.h>
+int main()
+{
+int number;
+printf("enter a number");
+scanf("%d",&number);
+if(number%==0)
+{
+printf("%d is even",number);
+}
+else
+{
+printf("%d is odd",number);
+}
+return 0;
+}
