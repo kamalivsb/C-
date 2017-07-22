2d59c0731d0e6997481462765a0f5f85f1fe46f1
kamalivsb <30316996+kamalivsb@users.noreply.github.com>
2017-07-22 17:47:15 +0530

    Create three.c

---
+++ three.c
@@ -0,0 +1,17 @@
+# C-
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+int largest;
+printf("enter thre numbers:");
+scanf("%d %d %d",&a,&b,&c);
+if(a>b && a>c)
+largest==a;
+else if(b>a && b>c)
+largest==b;
+else
+largest==c;
+printf("largest number is =%d",largest);
+return 0;
+}
